33ab76fcc20511903651f6ac7de77b07a2c358b7
SONU KOL <108781856+SONUKOL1@users.noreply.github.com>
2023-04-07 17:53:32 +0530

    Update SERIAL+BLINK.c

--- SERIAL+BLINK.c
+++ SERIAL+BLINK.c
@@ -3,25 +3,21 @@
 #include "stdio.H"
 #include "string.H"
 
-int putchar (int c)
-{
-	while (!TI);
-	TI = 0;
-	SBUF = c;
-	return c; 
+int putchar(int c) {
+    while (!TI);
+    TI = 0;
+    SBUF = c;
+    return c;
 }
 
 void main(void) {
     ALL_GPIO_QUASI_MODE;
-    MODIFY_HIRC(HIRC_24);
     UART_Open(24000000, UART0_Timer3, 115200);
     ENABLE_UART0_PRINTF;
-    TI = 1;
 
     while (1) {
         P14 = !P14;
-       printf_small("\n Hello world! , led status:%d",(P14));
-       // puts("how are you");
+        printf_fast_f("\nHello world!   LED status: %d", P14);
         Timer1_Delay10ms(100);
     }
 }
